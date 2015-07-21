4215b02f46735841b3c03a3499fcd687604374c5
Romain WEEGER <romain.weeger@gmail.com>
2015-07-21 11:08:03 +0200

    min

--- wjs/extension/Element/D3Clip/D3Clip.js
+++ wjs/extension/Element/D3Clip/D3Clip.js
@@ -7,13 +7,6 @@
   WjsProto.register('Element', 'D3Clip', {
     classExtends: 'Element\\D3WebCom',
 
-    /**
-     * @require JsMethod > cssVendorPrefix
-     */
-    initElement: function () {
-      this.cssTransformName = this.wjs.cssVendorPrefix('transform');
-    },
-
     renderDom: function (renderData) {
       this.__super('renderDom', arguments);
       // getWorldPosition() will execute updateMatrixWorld();
@@ -21,7 +14,7 @@
       // Dom may be disabled.
       if (this.dom) {
         // Convert to CSS.
-        this.dom.style[this.cssTransformName] = this.matrixToCss(this.object3d.matrixWorld);
+        this.dom.style[this.wjs.cssVendorPrefix('transform')] = this.matrixToCss(this.object3d.matrixWorld);
       }
     },
 
